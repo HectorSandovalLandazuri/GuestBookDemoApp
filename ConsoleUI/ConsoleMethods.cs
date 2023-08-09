388d778b0a37b3c32f80d27dfefae8cce9c3b72b
HectorSandovalLandazuri <88155761+HectorSandovalLandazuri@users.noreply.github.com>
2023-08-09 11:15:32 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleUI/ConsoleMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+	public static class ConsoleMethods
+	{
+
+		public static void WelcomeUser()
+		{
+			Console.WriteLine("*****************************");
+			Console.WriteLine("Welcome to the Guest Book App");
+            Console.WriteLine("*****************************");
+            Console.WriteLine();
+        }
+
+		public static string GetInfoFromConsole(string message)
+		{
+			Console.Write(message);
+			return Console.ReadLine();
+		}
+
+	}
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using Methods;
+
+Dictionary<string, int> guests = new Dictionary<string, int>();
+string moreGuests = "";
+string guest = "";
+int invited = 0;
+bool isValidInvited = false;
+string invitedText = "";
+int totalInvited = 0;
+
+ConsoleMethods.WelcomeUser();
+
+do
+{
+    guest = ConsoleMethods.GetInfoFromConsole("What's your name? ");
+    isValidInvited = false;
+    while (!isValidInvited)
+    {
+        invitedText = ConsoleMethods.GetInfoFromConsole("How many are you? ");
+        isValidInvited = int.TryParse(invitedText, out invited);
+    }
+    guests.Add(guest, invited);
+    moreGuests = ConsoleMethods.GetInfoFromConsole("Are there more guests (y/n)? ");
+    Console.WriteLine();
+} while (moreGuests.ToLower() != "n");
+
+foreach (var item in guests)
+{
+    Console.WriteLine($"{item.Key}: {item.Value} guests.");
+    totalInvited += item.Value;
+}
+Console.WriteLine();
+Console.WriteLine($"There are {totalInvited} people invited.");
+
+
+
+
